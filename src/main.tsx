45b465ead28615a9e268133797380452b6e42456
malsaslamcraft97 <aslam@incubyte.co>
2026-04-19 23:38:04 +0530

    feat(ui): add react dev tools and test the prefetch/background refetching locally

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./styles/global.scss";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const client = new QueryClient();
 
@@ -10,6 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
       <App />
+
+      {/* DevTools panel */}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,20 @@ import { SearchInput } from "../../components/filters/SearchInput/SearchInput";
 import { RegionFilter } from "../../components/filters/RegionFilter/RegionFilter";
 import { CountryGrid } from "../../components/countries/CountryGrid/CountryGrid";
 import styles from "./Home.module.scss";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { fetchCountries } from "../../api/countriesApi";
 
 export function Home() {
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    queryClient.prefetchQuery({
+      queryKey: ["countries"],
+      queryFn: fetchCountries,
+    });
+  }, [queryClient]);
+
   return (
     <div>
       <Header />
