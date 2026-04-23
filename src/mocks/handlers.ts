f0bb0a8a50d6f0210e765030caed855124624e48
malsaslamcraft97 <aslam@incubyte.co>
2026-04-23 12:08:47 +0530

    test(msw): update mockCountries having more data, generated in a loop

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,28 +1,12 @@
 import { http, HttpResponse } from "msw";
 
-export const mockCountries = [
-  {
-    name: { common: "India" },
-    population: 1400000000,
-    region: "Asia",
-    capital: ["New Delhi"],
-    flags: { png: "https://flagcdn.com/in.png" },
-  },
-  {
-    name: { common: "Germany" },
-    population: 83000000,
-    region: "Europe",
-    capital: ["Berlin"],
-    flags: { png: "https://flagcdn.com/de.png" },
-  },
-  {
-    name: { common: "Brazil" },
-    population: 210000000,
-    region: "Americas",
-    capital: ["Brasília"],
-    flags: { png: "https://flagcdn.com/br.png" },
-  },
-];
+export const mockCountries = Array.from({ length: 20 }).map((_, i) => ({
+  name: { common: `Country ${i}` },
+  population: 1000000 + i,
+  region: i % 2 === 0 ? "Asia" : "Europe",
+  capital: [`Capital ${i}`],
+  flags: { png: "https://flagcdn.com/in.png" },
+}));
 
 export const handlers = [
   http.get("https://restcountries.com/v3.1/all", () => {
